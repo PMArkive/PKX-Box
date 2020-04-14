e0b9f208800a340f6f21ac65b9db6130cdaa932e
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2020-04-14 07:20:48 -0500

    Temporarily remove web app

--- web-app/src/graphql/queries/collection-names.js
+++
@@ -1,7 +0,0 @@
-import gql from "graphql-tag";
-
-export const GET_COLLECTION_NAMES = gql`
-  query collectionNames($userId: String!) {
-    collectionNames(userId: $userId)
-  }
-`;
--- web-app/src/graphql/queries/is-nav-drawer-open.js
+++
@@ -1,7 +0,0 @@
-import gql from "graphql-tag";
-
-export const GET_IS_NAV_DRAWER_OPEN = gql`
-  query isNavDrawerOpen {
-    isNavDrawerOpen @client
-  }
-`;
