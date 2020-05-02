316fb7be73ebbb7329fd2c4d856421bc778be891
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2020-05-01 21:57:04 -0500

    Only use git has for app version

--- web-app/scripts/get-version.js
+++ web-app/scripts/get-version.js
@@ -1,8 +1,7 @@
-const version = require('../package.json').version;
 const commitHash = require('child_process')
   .execSync('git rev-parse HEAD')
   .toString()
   .trim()
   .slice(0, 7);
 
-console.log(`${version}-${commitHash}`);
+console.log(commitHash);
